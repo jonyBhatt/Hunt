9a87a5b8095014af6302e936a48a15a95d3d2137
Jony Bhattacharjee <jonybhattacharjee886@gmail.com>
2023-10-30 02:26:12 +0600

    sign up done

--- server/authAction.ts
+++ server/authAction.ts
@@ -1,6 +1,8 @@
 "use server";
 
+import prisma from "@/lib/db/prisma";
 import { CreateUser } from "@/types";
+import bcrypt from "bcrypt";
 
 export async function createUser({
   name,
@@ -8,5 +10,20 @@ export async function createUser({
   email,
   password,
 }: CreateUser) {
-  console.log(name, username, email, password);
+  // console.log(name, username, email, password);
+  const hashPass = await bcrypt.hash(password, 10);
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        username,
+        password: hashPass,
+      },
+    });
+
+    console.log(newUser);
+  } catch (error) {
+    console.log(error);
+  }
 }
